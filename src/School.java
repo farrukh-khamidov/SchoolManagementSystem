61d69138018fd3fde37d16343a6246676a9533c5
farrukh <mrfarrukh.94@gmail.com>
2024-12-25 20:20:04 +0500

    version 3 ready

--- src/School.java
+++ src/School.java
@@ -1,29 +1,14 @@
 public class School {
-    private Teacher[] teachers = new Teacher[2];
-    private Student[] students = new Student[2];
-    private int teacherCount = 0;
-    private int studentCount = 0;
+    private TeacherList teachers = new TeacherList();
+    private StudentList students = new StudentList();
     private int totalMoneyEarned = 0;
     private int totalMoneyPaid = 0;
     public void addTeacher(Teacher teacher) {
-        if (teacherCount >= teachers.length) {
-            Teacher[] newTeachers = new Teacher[teachers.length * 2];
-            for (int i = 0; i < teachers.length; i++) {
-                newTeachers[i] = teachers[i];
-            }
-            teachers = newTeachers;
-        }
-        teachers[teacherCount++] = teacher;
-
+        teachers.add(teacher);
     }
 
     public void addStudent(Student student) {
-        students[studentCount++] = student;
-        if (studentCount >= students.length) {
-            Student[] newStudents = new Student[students.length * 2];
-            System.arraycopy(students, 0, newStudents, 0, students.length);
-            students = newStudents;
-        }
+        students.add(student);
     }
 
     public int getTotalMoneyEarned() {
